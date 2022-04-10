b3f40e983440a2cce741c54d8ef2675a4b3087b3
unknown <ijimerusora69@gmail.com>
2022-04-11 06:32:24 +0800

    commit

--- src/js/other/sTime.js
+++ src/js/other/sTime.js
@@ -4,7 +4,7 @@ export default function sTime(){
     var time = new Date(); const h=time.getHours(), m=time.getMinutes(),s=time.getSeconds();
     let ampm=document.createElement("p"); 
     
-    h >= 12 ? ampm.innerHTML="pm":"am", h >= 0==24 ? ampm.innerHTML="am":"pm";
+    h == 12 ? ampm.innerHTML="pm":"am", h == 0 ? ampm.innerHTML="am":"pm";
     h <= 8 ? h2.innerHTML="breakfast":"absent", h <= 18 && ampm.innerHTML=="pm" ? h2.innerHTML="dinner":"absent";
     h >= 8 || h >= 12 || h >= 18 || h == 0 > 1 ? h2.innerHTML="idling":"absent";
     
